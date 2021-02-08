5f62eb3c179adfc2089e4dc1e05674cbd75ffb00
Anton <wizmager@gmail.com>
2021-02-08 16:56:32 +0300

    Task03 completed

---
+++ Task03/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Task03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSaveAs = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(80, 12);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 1;
+            this.btnConvert.Text = "Save";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnSaveAs);
+            this.panel1.Controls.Add(this.btnOpen);
+            this.panel1.Controls.Add(this.btnConvert);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 117);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(234, 44);
+            this.panel1.TabIndex = 4;
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Location = new System.Drawing.Point(156, 12);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveAs.TabIndex = 3;
+            this.btnSaveAs.Text = "Save As";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(3, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblStatus.Location = new System.Drawing.Point(0, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(234, 117);
+            this.lblStatus.TabIndex = 5;
+            this.lblStatus.Text = "Status line...";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 161);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(250, 200);
+            this.MinimumSize = new System.Drawing.Size(250, 200);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Converter";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ Task03/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Task03
+{
+    public partial class Form1 : Form
+    {
+        private List<Student> students;
+        private string path;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            if (students != null)
+            {
+                string[] pathStr = path.Split('.');
+                path = pathStr[0] + ".xml";
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Student>));
+                using (FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
+                {
+                    xmlSerializer.Serialize(fileStream, students);
+                }
+                lblStatus.Text = $"Список сохранён по адресу  {path} .";
+            }
+            else
+            {
+                MessageBox.Show("Вы не загрузили список.");
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog(){Filter = "Text files(*.csv)|*.csv"};
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                students = new List<Student>();
+                path = openFileDialog.FileName;
+                using (StreamReader sr = new StreamReader(openFileDialog.FileName))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] s = sr.ReadLine().Split(',', ';', ':');
+                        students.Add(new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]));
+                    }
+                }
+                lblStatus.Text = $"Список с адресом {openFileDialog.FileName} загружен.";
+            }
+        }
+
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            if (students != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog(){Filter = "Text files(*.xml)|*.xml"};
+
+                if(saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Student>));
+                    using (FileStream fileStream = new FileStream(saveFileDialog.FileName, FileMode.Create, FileAccess.Write))
+                    {
+                        xmlSerializer.Serialize(fileStream, students);
+                    }
+                }
+                lblStatus.Text = $"Список сохранён по адресу {saveFileDialog.FileName} .";
+            }
+            else
+            {
+                MessageBox.Show("Вы не загрузили список.");
+            }
+        }
+    }
+}
